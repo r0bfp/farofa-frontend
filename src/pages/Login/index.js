9c5db0539069e49dc7b0565c6a760757725df198
r0bfp <robfpinheiro@gmail.com>
2024-02-20 08:17:45 -0300

    refactor: now uses html5 form to manage form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,7 @@
 import './index.css'
 import { Form, Button } from 'react-bootstrap'
 import logo from '../../images/logo.png'
-import { useForm } from 'react-hook-form';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -10,11 +9,22 @@ import { toast } from 'react-toastify';
 
 export default function Login() {
     const {signin} = useContext(AuthContext)
-    const {register, handleSubmit} = useForm()
+    const [validated, setValidated] = useState(false)
     const navigate = useNavigate()
 
-    async function onSubmit(data) {
-        const status = await signin(data.username, data.password)
+    async function onSubmit(event) {
+        const form = event.currentTarget
+        const {username, password} = form.elements
+
+        event.preventDefault()
+        event.stopPropagation()
+
+        if(form.checkValidity() === false){
+            setValidated(true)
+            return
+        }
+
+        const status = await signin(username.value, password.value)
 
         if(status){
             navigate('/app/home')
@@ -32,16 +42,16 @@ export default function Login() {
                     </div>
                     <h3>Controle de Estoque</h3>
                 </div>
-                <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form noValidate validated={validated} onSubmit={onSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Usuário</Form.Label>
-                        <Form.Control required type="text" placeholder="Usuário" {...register('username', {required: true})}/>
-                        <Form.Control.Feedback type='invalid'>Campo obrigatório!</Form.Control.Feedback>
+                        <Form.Control required type="text" placeholder="Usuário" name='username'/>
+                        <Form.Control.Feedback type='invalid'>Usuário inválido!</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Senha</Form.Label>
-                        <Form.Control required type="password" placeholder="Senha" {...register('password', {required: true})}/>
-                        <Form.Control.Feedback type='invalid'>Campo obrigatório!</Form.Control.Feedback>
+                        <Form.Control required type="password" placeholder="Senha" name='password'/>
+                        <Form.Control.Feedback type='invalid'>Senha inválida!</Form.Control.Feedback>
                     </Form.Group>
                     <div className="d-grid gap-2">
                         <Button variant="primary" type="submit" className='w-12'>Entrar</Button>
