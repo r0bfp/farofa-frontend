08afa2c67153d0c6a1d979d8f8a8080fb7567b03
r0bfp <robfpinheiro@gmail.com>
2024-02-19 21:16:21 -0300

    feat: add products page

---
+++ src/pages/Products/index.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { FarofaApi } from '../../services/FarofaApi'
+import ProductsTable from './ProductsTable'
+import SettingsDropdown from './SettingsDropdown'
+import './style.css'
+
+
+export default function Products() {
+    const [products, setProducts] = useState([])
+    const [yampiProducts, setYampiProducts] = useState([])
+
+    useEffect(() => {
+        fetchProducts()
+        fetchYampiProducts()
+    }, [])
+
+    async function fetchProducts() {
+        setProducts(await FarofaApi.listProducts())
+    }
+
+    async function fetchYampiProducts() {
+        setYampiProducts(await FarofaApi.listYampiProducts())
+    }
+
+    return (
+        <div className='container'>
+            <div className='products-module'>
+                <div className='container-header'>
+                    <div>
+                        <h5>Produtos</h5>
+                        <span className='text-secondary'>Gerenciamento do estoque de produtos</span>
+                    </div>
+                    <div>
+                        <SettingsDropdown />
+                    </div>
+                </div>
+                <div className='container-body'>
+                    <ProductsTable products={products} setProducts={setProducts} yampiProducts={yampiProducts}/>
+                </div>
+                <div className='container-footer'>
+                    <span className='text-secondary'>{products.length} Registros</span>
+                </div>
+            </div>
+        </div>
+    )
+}
